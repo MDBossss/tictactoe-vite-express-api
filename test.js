e492f36d25bfe54f808490bcaa5e97051ecb4695
MDBossss <ferrari020203@gmail.com>
2023-02-11 11:16:37 +0100

    env

--- test.js
+++ test.js
@@ -1,10 +1,4 @@
-require('dotenv').config();
-const express = require("express");
-const app = express();
-const http = require("http").Server(app);
-
-const port = process.env.PORT || 5000;
-const io = require("socket.io")(http);
+const io = require("socket.io")(5000,{cors: {origin:"*"}});
 
 
 io.on("connection", (socket) => {
@@ -20,6 +14,3 @@ io.on("connection", (socket) => {
     })
 
 });
-
-
-http.listen(port, () => console.log("server running on port" + port));
